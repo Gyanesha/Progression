ca1af801f3b29965f126dd980e105a128b2b8ebb
Gyanesha <gyaneshaprajjwal@gmail.com>
2019-05-24 15:06:46 +0530

    Linking User objects with the view and template

--- Progress/dashboard/views.py
+++ Progress/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def user_list(request):
-	return render(request, 'dashboard/user_list.html',{})
+	return render(request, 'dashboard/user_list.html',{"users": User.objects.all})
